a7c48a7f88580a27f18a2ddf77d9941ebf49dc7d
Shinohara Teruki <ts_3156@yahoo.co.jp>
2014-03-16 16:24:39 +0900

    implement get_all_tweets

--- lib/ex_twitter.rb
+++ lib/ex_twitter.rb
@@ -8,6 +8,19 @@ class ExTwitter < Twitter::REST::Client
     super
   end
 
+def collect_with_max_id(collection=[], max_id=nil, &block)
+  response = yield(max_id)
+  collection += response
+  response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
+end
+
+def get_all_tweets(user)
+  collect_with_max_id do |max_id|
+    options = {count: 200, include_rts: true}
+    options[:max_id] = max_id unless max_id.nil?
+    user_timeline(user, options)
+  end
+end
 end
 
 if __FILE__ == $0
