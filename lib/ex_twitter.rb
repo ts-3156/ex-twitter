c7b28b7258cdcf450b2b1d172e541809779596b3
Shinohara Teruki <ts_3156@yahoo.co.jp>
2014-12-16 02:21:18 +0900

    refactoring

--- lib/ex_twitter.rb
+++ lib/ex_twitter.rb
@@ -6,7 +6,7 @@ require 'parallel'
 
 # extended twitter
 class ExTwitter < Twitter::REST::Client
-  attr_accessor :cache, :cache_expires_in, :max_attempts, :wait
+  attr_accessor :cache, :cache_expires_in, :max_attempts, :wait, :auto_paginate
 
   MAX_ATTEMPTS = 1
   WAIT = false
@@ -32,6 +32,7 @@ class ExTwitter < Twitter::REST::Client
       config = self.class.get_config_value(yml_config)
     end
 
+    self.auto_paginate = (config[:auto_paginate] || true)
     self.max_attempts = (config[:max_attempts] || MAX_ATTEMPTS)
     self.wait = (config[:wait] || WAIT)
     self.cache_expires_in = (config[:cache_expires_in] || CACHE_EXPIRES_IN)
@@ -54,280 +55,108 @@ class ExTwitter < Twitter::REST::Client
     false
   end
 
-  def collect_with_max_id(collection=[], max_id=nil, &block)
-    response = yield(max_id)
-    return response unless response[1].nil?
+  # githubでは、レスポンスが次のエンドポイントを知っているため、ブロックをデータの結合のみに使っている。引数ではAPI名を渡している。
+  # 一方、twitterでは、レスポンスが次のエンドポイントを知らないため、ブロック内にAPI名を持ち、再帰的にブロックを呼び出している。
+  # また、再帰を使うため、引数をコレクションの引き渡しに使ってしまっている。(この問題については通常のループを使えば解決する)
+  #
+  # APIは通常繰り返しの呼び出しになるため、API名は常に同じものを保持しておき、ブロックはデータの結合に使った方が無駄がなく自由度が高い。
+  # また、再帰ではなく通常のループを使った方が分かりやすい。
+  #
+  # このgithub方式を実現するためには、メソッド名(例：user_timeline)を渡し、ループさせるか、user_timelineが内部で使っている
+  # objects_from_response_with_userをループさせればよい。
+  #
+  # この2種類の方法を比較した場合、より外側のレイヤーを使った方が、使うライブラリの内部情報に依存しなくなるため、好ましい。
+  #
+  # twitterで再帰方式がとられている理由は、おそらく、一般ユーザー向けのサンプルでメソッド名を渡すようなリフレクションを避けるため、
+  # なのかもしれない。
+  def collect_with_max_id(method_name, args, &block)
+    options = args.pop
+    max_calls = options.delete(:max_calls) || 3
+    data = last_response = send(method_name, *args, options)
 
-    collection += response[0]
-    response[0].empty? ? [collection.flatten, nil] : collect_with_max_id(collection, response[0].last.id - 1, &block)
+    if auto_paginate
+      (max_calls - 1).times do
+        break unless last_response.any?
+
+        options[:max_id] = last_response.last.id - 1
+        last_response = send(method_name, *args, options)
+        if block_given?
+          yield(data, last_response)
+        else
+          data.concat(last_response) if last_response.is_a?(Array)
+        end
+      end
+    end
+
+    data
   end
 
-  def collect_with_cursor(collection=[], cursor=-1, &block)
-    response = yield(cursor)
-    return response unless response[1].nil?
+  def collect_with_cursor(method_name, args, &block)
+    options = args.pop
+    max_calls = options.delete(:max_calls) || 3
+    last_response = send(method_name, *args, options).attrs
+    data = last_response[:users] || last_response[:ids]
 
-    collection += (response[0][:users] || response[0][:ids])
-    next_cursor = response[0][:next_cursor]
-    next_cursor == 0 ? [collection.flatten, nil] : collect_with_cursor(collection, next_cursor, &block)
+    if auto_paginate
+      (max_calls - 1).times do
+        next_cursor = last_response[:next_cursor]
+        break if !next_cursor || next_cursor == 0
+
+        options[:cursor] = next_cursor
+        last_response = send(method_name, *args, options).attrs
+        if block_given?
+          yield(data, last_response)
+        else
+          items = last_response[:users] || last_response[:ids]
+          data.concat(items) if items.is_a?(Array)
+        end
+      end
+    end
+
+    data
   end
 
-  def get_latest_200_tweets(user=nil)
-    num_attempts = 0
+  alias :old_user_timeline :user_timeline
+  def user_timeline(user = nil)
     options = {count: 200, include_rts: true}
-    begin
-      num_attempts += 1
-      [user_timeline(user, options), nil]
-    rescue Twitter::Error::TooManyRequests => e
-      if num_attempts <= MAX_ATTEMPTS
-        if WAIT
-          sleep e.rate_limit.reset_in
-          retry
-        else
-          puts "retry #{e.rate_limit.reset_in} seconds later"
-          [[], e]
-        end
-      else
-        puts "fail. num_attempts > MAX_ATTEMPTS(=#{MAX_ATTEMPTS})"
-        [[], e]
-      end
-    rescue => e
-      if num_attempts <= MAX_ATTEMPTS
-        retry
-      else
-        puts "fail. num_attempts > MAX_ATTEMPTS(=#{MAX_ATTEMPTS}), something error #{e.inspect}"
-        [[], e]
-      end
-    end
+    collect_with_max_id(:old_user_timeline, [user, options])
   end
 
-  def get_all_tweets(user=nil)
-    num_attempts = 0
-    collect_with_max_id do |max_id|
-      options = {count: 200, include_rts: true}
-      options[:max_id] = max_id unless max_id.nil?
-      begin
-        num_attempts += 1
-        [user_timeline(user, options), nil]
-      rescue Twitter::Error::TooManyRequests => e
-        if num_attempts <= MAX_ATTEMPTS
-          if WAIT
-            sleep e.rate_limit.reset_in
-            retry
-          else
-            puts "retry #{e.rate_limit.reset_in} seconds later"
-            [[], e]
-          end
-        else
-          puts "fail. num_attempts > MAX_ATTEMPTS(=#{MAX_ATTEMPTS})"
-          [[], e]
-        end
-      rescue => e
-        if num_attempts <= MAX_ATTEMPTS
-          retry
-        else
-          puts "fail. num_attempts > MAX_ATTEMPTS(=#{MAX_ATTEMPTS}), something error #{e.inspect}"
-          [[], e]
-        end
-      end
-    end
+  alias :old_friends :friends
+  def friends(user = nil)
+    options = {count: 200, include_user_entities: true}
+    collect_with_cursor(:old_friends, [user, options])
   end
 
-  def get_all_friends(user=nil)
-    num_attempts = 0
-    collect_with_cursor do |cursor|
-      options = {count: 200, include_user_entities: true}
-      options[:cursor] = cursor unless cursor.nil?
-      cache_key = "#{self.class.name}:#{__callee__}:#{user}:#{options}"
-
-      cache = self.read(cache_key)
-      next [cache, nil] unless cache.nil?
-      begin
-        num_attempts += 1
-        object = friends(user, options)
-        self.write(cache_key, object.attrs)
-        [object.attrs, nil]
-      rescue Twitter::Error::TooManyRequests => e
-        if num_attempts <= MAX_ATTEMPTS
-          if WAIT
-            sleep e.rate_limit.reset_in
-            retry
-          else
-            puts "retry #{e.rate_limit.reset_in} seconds later"
-            [{}, e]
-          end
-        else
-          puts "fail. num_attempts > MAX_ATTEMPTS(=#{MAX_ATTEMPTS})"
-          [{}, e]
-        end
-      rescue => e
-        if num_attempts <= MAX_ATTEMPTS
-          retry
-        else
-          puts "fail. num_attempts > MAX_ATTEMPTS(=#{MAX_ATTEMPTS}), something error #{e.inspect}"
-          [{}, e]
-        end
-      end
-    end
+  alias :old_followers :followers
+  def followers(user = nil)
+    options = {count: 200, include_user_entities: true}
+    collect_with_cursor(:old_followers, [user, options])
   end
 
-  def get_all_followers(user=nil)
-    num_attempts = 0
-    collect_with_cursor do |cursor|
-      options = {count: 200, include_user_entities: true}
-      options[:cursor] = cursor unless cursor.nil?
-      cache_key = "#{self.class.name}:#{__callee__}:#{user}:#{options}"
-
-      cache = self.read(cache_key)
-      next [cache, nil] unless cache.nil?
-      begin
-        num_attempts += 1
-        object = followers(user, options)
-        self.write(cache_key, object.attrs)
-        [object.attrs, nil]
-      rescue Twitter::Error::TooManyRequests => e
-        if num_attempts <= MAX_ATTEMPTS
-          if WAIT
-            sleep e.rate_limit.reset_in
-            retry
-          else
-            puts "retry #{e.rate_limit.reset_in} seconds later"
-            [{}, e]
-          end
-        else
-          puts "fail. num_attempts > MAX_ATTEMPTS(=#{MAX_ATTEMPTS})"
-          [{}, e]
-        end
-      rescue => e
-        if num_attempts <= MAX_ATTEMPTS
-          retry
-        else
-          puts "fail. num_attempts > MAX_ATTEMPTS(=#{MAX_ATTEMPTS}), something error #{e.inspect}"
-          [{}, e]
-        end
-      end
-    end
+  alias :old_friend_ids :friend_ids
+  def friend_ids(user = nil)
+    options = {count: 5000}
+    collect_with_cursor(:old_friend_ids, [user, options])
   end
 
-  def get_all_friend_ids(user=nil)
-    num_attempts = 0
-    collect_with_cursor do |cursor|
-      options = {count: 5000}
-      options[:cursor] = cursor unless cursor.nil?
-      cache_key = "#{self.class.name}:#{__callee__}:#{user}:#{options}"
-
-      cache = self.read(cache_key)
-      next [cache, nil] unless cache.nil?
-      begin
-        num_attempts += 1
-        object = friend_ids(user, options)
-        self.write(cache_key, object.attrs)
-        [object.attrs, nil]
-      rescue Twitter::Error::TooManyRequests => e
-        if num_attempts <= MAX_ATTEMPTS
-          if WAIT
-            sleep e.rate_limit.reset_in
-            retry
-          else
-            puts "retry #{e.rate_limit.reset_in} seconds later"
-            [{}, e]
-          end
-        else
-          puts "fail. num_attempts > MAX_ATTEMPTS(=#{MAX_ATTEMPTS})"
-          [{}, e]
-        end
-      rescue => e
-        if num_attempts <= MAX_ATTEMPTS
-          retry
-        else
-          puts "fail. num_attempts > MAX_ATTEMPTS(=#{MAX_ATTEMPTS}), something error #{e.inspect}"
-          [{}, e]
-        end
-      end
-    end
+  alias :old_follower_ids :follower_ids
+  def follower_ids(user = nil)
+    options = {count: 5000}
+    collect_with_cursor(:old_follower_ids, [user, options])
   end
 
-  def get_all_follower_ids(user=nil)
-    num_attempts = 0
-    collect_with_cursor do |cursor|
-      options = {count: 5000}
-      options[:cursor] = cursor unless cursor.nil?
-      cache_key = "#{self.class.name}:#{__callee__}:#{user}:#{options}"
-
-      cache = self.read(cache_key)
-      next [cache, nil] unless cache.nil?
-      begin
-        num_attempts += 1
-        object = follower_ids(user, options)
-        self.write(cache_key, object.attrs)
-        [object.attrs, nil]
-      rescue Twitter::Error::TooManyRequests => e
-        if num_attempts <= MAX_ATTEMPTS
-          if WAIT
-            sleep e.rate_limit.reset_in
-            retry
-          else
-            puts "retry #{e.rate_limit.reset_in} seconds later"
-            [{}, e]
-          end
-        else
-          puts "fail. num_attempts > MAX_ATTEMPTS(=#{MAX_ATTEMPTS})"
-          [{}, e]
-        end
-      rescue => e
-        if num_attempts <= MAX_ATTEMPTS
-          retry
-        else
-          puts "fail. num_attempts > MAX_ATTEMPTS(=#{MAX_ATTEMPTS}), something error #{e.inspect}"
-          [{}, e]
-        end
-      end
-    end
-  end
-
-  def get_users(ids)
-    ids_per_worker = []
-    while(ids.size > 0)
-      ids_per_worker << ids.slice!(0, [100, ids.size].min)
-    end
-
-    num_attempts = 0
+  alias :old_users :users
+  def users(ids)
+    ids_per_worker = ids.each_slice(100).to_a
     processed_users = []
+
     Parallel.each_with_index(ids_per_worker, in_threads: ids_per_worker.size) do |ids, i|
-      cache_key = "#{self.class.name}:#{__callee__}:#{i}:#{ids}"
-      cache = self.read(cache_key)
-      if cache.nil?
-        begin
-          num_attempts += 1
-          object = {i: i, users: users(ids)}
-          self.write(cache_key, object)
-          processed_users << object
-        rescue Twitter::Error::TooManyRequests => e
-          if num_attempts <= MAX_ATTEMPTS
-            if WAIT
-              sleep e.rate_limit.reset_in
-              retry
-            else
-              puts "retry #{e.rate_limit.reset_in} seconds later"
-              {i: i, users: [], e: e}
-            end
-          else
-            puts "fail. num_attempts > MAX_ATTEMPTS(=#{MAX_ATTEMPTS})"
-            {i: i, users: [], e: e}
-          end
-        rescue => e
-          if num_attempts <= MAX_ATTEMPTS
-            retry
-          else
-            puts "fail. num_attempts > MAX_ATTEMPTS(=#{MAX_ATTEMPTS}), something error #{e.inspect}"
-            {i: i, users: [], e: e}
-          end
-        end
-      else
-        processed_users << cache
-      end
+      _users = {i: i, users: old_users(ids)}
+      processed_users << _users
     end
-    # TODO remove if users have error, or raise
-    processed_users.sort_by{|p|p[:i]}.map{|p|p[:users]}.flatten
+
+    processed_users.sort_by{|p| p[:i] }.map{|p| p[:users] }.flatten
   end
 
   # mentions_timeline is to fetch the timeline of Tweets mentioning the authenticated user
@@ -372,25 +201,6 @@ class ExTwitter < Twitter::REST::Client
       end
     end
   end
-end
-
-if __FILE__ == $0
-  puts '--start--'
-  client = ExTwitter.new
-  puts client.get_all_friends.size
-  #followers, error = client.get_all_followers
-  #puts "#{followers.size} #{error.inspect}"
-  # tweets, error = client.search_japanese_tweets('りんご')
-  # tweets, error = client.search_tweets_except_rt('#りんご')
-  # puts tweets.size
-=begin
-  tweets.each do |t|
-    next if !t.media?
-    puts t.text
-    puts t.media[0].attrs
-    puts '----'
-  end
-=end
 end
 
 
