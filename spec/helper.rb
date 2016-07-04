76736675ff2e5d46d5cbc354de5e3d9e9746772e
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-07-05 02:19:26 +0900

    cp fixtures from twitter gem

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,9 +1,66 @@
 require 'twitter_with_auto_pagination'
 require 'rspec'
+require 'stringio'
+require 'tempfile'
+require 'timecop'
+require 'webmock/rspec'
+
+require_relative 'support/media_object_examples'
 
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
-  config.order = 'random'
+end
+
+def a_delete(path)
+  a_request(:delete, Twitter::REST::Request::BASE_URL + path)
+end
+
+def a_get(path)
+  a_request(:get, Twitter::REST::Request::BASE_URL + path)
+end
+
+def a_post(path)
+  a_request(:post, Twitter::REST::Request::BASE_URL + path)
+end
+
+def a_put(path)
+  a_request(:put, Twitter::REST::Request::BASE_URL + path)
+end
+
+def stub_delete(path)
+  stub_request(:delete, Twitter::REST::Request::BASE_URL + path)
+end
+
+def stub_get(path)
+  stub_request(:get, Twitter::REST::Request::BASE_URL + path)
+end
+
+def stub_post(path)
+  stub_request(:post, Twitter::REST::Request::BASE_URL + path)
+end
+
+def stub_put(path)
+  stub_request(:put, Twitter::REST::Request::BASE_URL + path)
+end
+
+def fixture_path
+  File.expand_path('../fixtures', __FILE__)
+end
+
+def fixture(file)
+  File.new(fixture_path + '/' + file)
+end
+
+def capture_warning
+  begin
+    old_stderr = $stderr
+    $stderr = StringIO.new
+    yield
+    result = $stderr.string
+  ensure
+    $stderr = old_stderr
+  end
+  result
 end
