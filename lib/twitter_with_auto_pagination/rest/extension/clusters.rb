49e3a86ebf3573dbf18728a10e06ad8d1a20a8ea
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-10-01 15:56:13 +0900

    leave words consisted of hiragana

--- lib/twitter_with_auto_pagination/rest/extension/clusters.rb
+++ lib/twitter_with_auto_pagination/rest/extension/clusters.rb
@@ -6,32 +6,41 @@ module TwitterWithAutoPagination
       module Clusters
         include TwitterWithAutoPagination::REST::Utils
 
-        def clusters_belong_to(text)
+        # @param text [String] user_timeline.map(&:text).join(' ')
+        def clusters_belong_to(text, limit: 10)
           return {} if text.blank?
 
-          exclude_words = JSON.parse(File.read(Rails.configuration.x.constants['cluster_bad_words_path']))
-          special_words = JSON.parse(File.read(Rails.configuration.x.constants['cluster_good_words_path']))
+          if defined?(Rails)
+            exclude_words = JSON.parse(File.read(Rails.configuration.x.constants['cluster_bad_words_path']))
+            special_words = JSON.parse(File.read(Rails.configuration.x.constants['cluster_good_words_path']))
+          else
+            exclude_words = JSON.parse(File.read('./cluster_bad_words.json'))
+            special_words = JSON.parse(File.read('./cluster_good_words.json'))
+          end
+
+          %w(べたら それとも たしかに さそう そんなに ったことある してるの しそうな おやくま ってますか これをやってるよ のせいか).each { |w| exclude_words << w }
+          %w(面白い 可愛い 食べ物 宇多田ヒカル ご飯 面倒 体調悪くなる 空腹 頑張ってない 眼鏡 台風 沖縄 らんま1/2 女の子 怪我 足のむくみ 彼女欲しい 彼氏欲しい 吐き気 注射 海鮮チヂミ 出勤 価格ドットコム 幹事 雑談 パズドラ ビオフェルミン 餃子 お金 まんだらけ 結婚 焼肉 タッチペン).each { |w| special_words << w }
 
           # クラスタ用の単語の出現回数を記録
-          cluster_word_counter =
+          frequency =
             special_words.map { |sw| [sw, text.scan(sw)] }
-              .delete_if { |item| item[1].empty? }
-              .each_with_object(Hash.new(1)) { |item, memo| memo[item[0]] = item[1].size }
+              .delete_if { |_, matched| matched.empty? }
+              .each_with_object(Hash.new(0)) { |(word, matched), memo| memo[word] = matched.size }
 
           # 同一文字種の繰り返しを見付ける。漢字の繰り返し、ひらがなの繰り返し、カタカナの繰り返し、など
-          text.scan(/[一-龠〆ヵヶ々]+|[ぁ-んー～]+|[ァ-ヴー～]+|[ａ-ｚＡ-Ｚ０-９]+|[、。！!？?]+/).
+          text.scan(/[一-龠〆ヵヶ々]+|[ぁ-んー～]+|[ァ-ヴー～]+|[ａ-ｚA-ZＡ-Ｚ０-９]+|[、。！!？?]+/).
 
             # 複数回繰り返される文字を除去
             map { |w| w.remove /[？！?!。、ｗ]|(ー{2,})/ }.
 
-            # 文字数の少なすぎる単語、ひらがなだけの単語、除外単語を除去する
-            delete_if { |w| w.length <= 1 || (w.length <= 2 && w =~ /^[ぁ-んー～]+$/) || exclude_words.include?(w) }.
+            # 文字数の少なすぎる単語、除外単語を除去する
+            delete_if { |w| w.length <= 2 || exclude_words.include?(w) }.
 
             # 出現回数を記録
-            each { |w| cluster_word_counter[w] += 1 }
+            each { |w| frequency[w] += 1 }
 
           # 複数個以上見付かった単語のみを残し、出現頻度順にソート
-          cluster_word_counter.select { |_, v| v > 3 }.sort_by { |_, v| -v }.to_h
+          frequency.select { |_, v| 2 < v }.sort_by { |_, v| -v }.slice(0, limit).to_h
         end
 
         def clusters_assigned_to
